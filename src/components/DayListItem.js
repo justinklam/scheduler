722e2a9c7d115ea2ba77860e676490b4f2da6715
Justin Lam <justin.kh.lam@gmail.com>
2021-11-09 22:14:38 -0500

    add classNames logic to li in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,13 +3,13 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const dayClass = classNames("button", {
-    "button--confirm": props.confirm,
-    "button--danger": props.danger
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
   });
 
   return (
-    <li className="dayClass" onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
