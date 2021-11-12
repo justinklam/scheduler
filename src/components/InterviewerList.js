c7b67a1c72b8b292cbf26b3d25addf9f654c18b1
Justin Lam <justin.kh.lam@gmail.com>
2021-11-12 18:50:51 -0500

    change name to reflect value/onChange naming conventions

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,14 +3,16 @@ import InterviewerListItem from './InterviewerListItem';
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
+  console.log('INTERVIEWER PROPS-----', props)
   const interviewers = props.interviewers.map(interviewer => {
+    console.log('INTERVIEWER ITEM-----', interviewer)
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
