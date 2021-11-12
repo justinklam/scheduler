d16759846861976bbf731c2137fb144599691fa5
Justin Lam <justin.kh.lam@gmail.com>
2021-11-12 18:21:53 -0500

    remove console.logs used for testing

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,9 +2,7 @@ import React from "react";
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
-  console.log('props-----daylist', props);
   const days = props.days.map(day => {
-    // console.log('dayList DAY-----', day)
     return (
       <DayListItem
         key={day.id}
