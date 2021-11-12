e2f7d815b629a18b2ccd6ce764f93f1691734a7e
Justin Lam <justin.kh.lam@gmail.com>
2021-11-12 18:20:38 -0500

    changfe DayList.js to reflect new naming conventions to take in onChange and props.value

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,15 +2,17 @@ import React from "react";
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
+  console.log('props-----daylist', props);
   const days = props.days.map(day => {
+    // console.log('dayList DAY-----', day)
     return (
       <DayListItem
         key={day.id}
         name={day.name}
         spots={day.spots}
-        setDay={props.setDay}
-        selected={props.day === day.name}
-      />)
+        setDay={() => props.onChange(day.name)}
+        selected={day.name === props.value}
+        />)
   });
 
   return (
