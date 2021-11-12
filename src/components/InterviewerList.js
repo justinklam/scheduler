48bf2fcb29617feb35f80122ea818039e482d940
Justin Lam <justin.kh.lam@gmail.com>
2021-11-11 23:02:11 -0500

    initialize InterviewerList.js with function logic

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={props.setInterviewer}
+
+      />)
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">{interviewers}</h4>
+      <ul className="interviewers__list"></ul>
+    </section>  );
+}
