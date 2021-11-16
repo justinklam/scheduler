b6794770f06d286e55de6868137d2804220a1e05
Justin Lam <justin.kh.lam@gmail.com>
2021-11-15 22:36:39 -0500

    initialize Form.js and add in functionality to Cancel/Save buttons

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Button from '../Button';
+import InterviewerList from '../InterviewerList';
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null)  
+  };
+
+  const cancel = function() {
+    reset();
+    props.onCancel();
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={student}
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          value={interviewer}
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          {/* onSave has to be passed as an anom function due to how it takes in arguments */}
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
