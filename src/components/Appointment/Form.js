2f68743bce78e63b949cf35a0c13a31b80b68396
Justin Lam <justin.kh.lam@gmail.com>
2021-11-29 23:52:18 -0500

    remove excess spaces

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,7 +8,7 @@ export default function Form(props) {
 
   const reset = function() {
     setStudent("");
-    setInterviewer(null)  
+    setInterviewer(null)
   };
 
   const cancel = function() {
