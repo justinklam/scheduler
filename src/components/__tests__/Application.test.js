404e432735605d2e9f239bc46505803dbbf23785
Justin Lam <justin.kh.lam@gmail.com>
2021-12-01 00:46:36 -0500

    add in comments. wrap tests into a describe function

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,31 @@
 import React from "react";
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
 
 import { render, cleanup } from "@testing-library/react";
-
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
 import Application from "components/Application";
+import Appointment from "components/Appointment";
 
+/*
+  We import the component that we are testing
+*/
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+// xit to skip a test instead of it
+
+describe("Appointment", () => {
+
+  it("renders Application without crashing", () => {
+    render(<Application />);
+  });
+
+  it("renders Appointment without crashing", () => {
+    render(<Appointment />);
+  });
+
 });
