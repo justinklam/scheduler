f16ba6b0c5694a31b1c190f9333fdf28ada638b2
Justin Lam <justin.kh.lam@gmail.com>
2021-12-04 19:29:26 -0500

    add an integration test for Add/Saving a new appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,7 +3,9 @@ import React from "react";
   We are rendering `<Application />` down below, so we need React.createElement
 */
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, 
+  getByText, getAllByTestId, getByAltText, getByPlaceholderText } 
+  from "@testing-library/react";
 /*
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
@@ -16,11 +18,7 @@ import Appointment from "components/Appointment";
 */
 afterEach(cleanup);
 
-describe("Appointment", () => {
-
-  // it("renders Application without crashing", () => {
-  //   render(<Application />);
-  // });
+describe("Application", () => {
 
   it("defaults to Monday and changes the schedule when a new day is selected", () => {
     const { getByText } = render(<Application />);
@@ -29,6 +27,26 @@ describe("Appointment", () => {
       fireEvent.click(getByText("Tuesday"));
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
+    fireEvent.click(getByAltText(appointment, "Add"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    console.log(prettyDOM(appointment));
   });
 
 });
