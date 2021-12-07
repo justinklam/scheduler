faef6ea4dbf1fcbfa1078c2df8ac71b8ddd4e8a9
Justin Lam <justin.kh.lam@gmail.com>
2021-12-06 20:16:36 -0500

    implement additional test1 for Application.test.js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -61,4 +61,56 @@ describe("Application", () => {
     // console.log(prettyDOM(day));
   });
 
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    
+    fireEvent.click(getByAltText(appointment, "Add"));
+    
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+        
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    
+    fireEvent.click(getByText(appointment, "Save"));
+    
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    
+    const day = getAllByTestId(container, "day").find(element => queryByText(element, "Monday"));
+    expect(getByText(day, "no spots remaining"));
+
+  
+    // 3. Click the "Add" button on the first empty appointment.
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    // 5. Click the first interviewer in the list.
+    // 6. Click the "Save" button on that same appointment.
+    // 7. Check that the element with the text "Saving" is displayed.
+    // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
+    // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+  });
+
+  xit("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {
+    const { container } = render(<Application />);
+
+    // console.log(prettyDOM(container));
+
+
+  });
+
+  xit("shows the save error when failing to save an appointment", () => {
+
+  });
+
+  xit("shows the delete error when failing to delete an existing appointment", () => {
+
+  });
+
 });
