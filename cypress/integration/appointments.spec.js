2122e69d46f97d85879280aa5ae4f04331a017b1
Justin Lam <justin.kh.lam@gmail.com>
2021-12-11 14:19:54 -0500

    add additional 'book interview' test logic

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,8 +1,35 @@
 describe("appointments", () => {
 
-  it("should book an interview", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+  
     cy.visit("/");
+  
     cy.contains("Monday");
+   });
+
+  it("should book an interview", () => {
+
+    cy.get("[alt=Add]")
+    // finds the first Add on page, [0] is in use
+      .first()
+      .click();
+
+    cy.get("[data-testid='student-name-input']")
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
+    
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+  
   });
+
+  // find the add button in the second appointment slot.
+  // click the add button in the second appointment through alt tag
 
 });
